ac11a1d687f1c889a881fa535d8f133f02c2e662
Nikolay Noskov <nikolay.noskov@ardecs.com>
2020-02-27 10:50:49 +0400

    activemq spring boot refactoring

--- src/com/company/MessageListener.java
+++ src/com/company/MessageListener.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +9,7 @@ public class MessageListener {
     private final static String MESSAGE_QUEUE = "messageQueue";
 
     //    @JmsListener(destination = MESSAGE_QUEUE, containerFactory = "jmsFactory")
-//    @JmsListener(destination = MESSAGE_QUEUE)
+    @JmsListener(destination = MESSAGE_QUEUE)
     public void receiveMessage(String message) {
         System.out.println(message);
     }
